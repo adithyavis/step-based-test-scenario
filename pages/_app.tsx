6916e9bed6d05e0b1e04ccc22f1875cf3ebac7d3
Kanitkorn S <k.sujautra@gmail.com>
2020-10-10 04:33:14 +0900

    Add Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
 
 // import App from "next/app";
-import type { AppProps /*, AppContext */ } from 'next/app'
+import type { AppProps /*, AppContext */ } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
