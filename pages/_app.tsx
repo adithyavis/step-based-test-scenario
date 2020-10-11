198ae65de1a8a1131bb99a5d9b66549134cb0dba
Kanitkorn S <k.sujautra@gmail.com>
2020-10-11 19:05:19 +0900

    Add styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,34 @@
-import "../styles/globals.css"
-
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from "next/app"
 
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 // Only uncomment this method if you have blocking data requirements for
