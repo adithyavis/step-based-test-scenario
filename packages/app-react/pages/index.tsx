6df826916f28ed8bf8363e53b7d1120b1e4e86ac
Kanitkorn S <k.sujautra@gmail.com>
2020-10-28 01:42:39 +0900

    Update page title

--- packages/app-react/pages/index.tsx
+++ packages/app-react/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home({
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Autify - Frontend Interview Assignment</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main>
